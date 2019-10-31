7f6ad97a3f4cce6d38bc93ffadaa6e5b0e5e788f
Oleksii <o_oleksyshyn@ukr.net>
2019-10-31 21:43:48 +0200

    Added solutions for task 11

--- src/11_container_with_most_water.cpp
+++ src/11_container_with_most_water.cpp
@@ -1,10 +1,25 @@
-#include <iostream>
-#include <string>
+//
+// Created by Oleksyshyn Oleksii on 31.10.19.
+//
+
+/*
+ * Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai).
+ * n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0).
+ * Find two lines, which together with x-axis forms a container, such that the container contains the most water.
+ *
+ * Note: You may not slant the container and n is at least 2.
+ *
+ *
+ * Solution: two side approach -- O(n) in time, O(1) in memory
+ */
+
+
+
 #include <vector>
-#include <algorithm>
 
 
 using namespace std;
+
 
 class Solution {
 public:
@@ -28,10 +43,3 @@ public:
         return maxArea;
     }
 };
-
-int main() {
-    Solution s;
-    vector<int> x = {1, 8, 6, 2, 5, 4, 8, 3, 7};
-    std::cout << s.maxArea(x);
-    return 0;
-}
